77a2669002a0235014bc0b3fad365e4bb33dc2a3
MCCLAB560WD22 <kylie.holtorf.03@gmail.com>
2025-05-01 12:47:23 -0700

    Progress

--- src/main/java/edu/miracosta/cs112/finalproject/finalproject/Zombie.java
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/Zombie.java
@@ -19,5 +19,18 @@ public class Zombie extends Enemy {
 
     /********** SETTERS **********/
 
+    public boolean setRegen(int regen) {
+        if (regen >=0 && regen <= 1) {
+            this.regen = regen;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean setAll(int hp, Weapon weapon, int regen) {
+        return super.setAll(hp, weapon) && this.setRegen(regen);
+    }
+
 
 }
