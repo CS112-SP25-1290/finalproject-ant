a8c85c47f96b28d46a6c33102b9be0624abf147d
MCCLAB560WD23 <sdr.sdr@icloud.com>
2025-05-08 12:13:26 -0700

    Steve Class

---
+++ src/main/java/edu/miracosta/cs112/finalproject/finalproject/Steve.java
@@ -0,0 +1,41 @@
+package edu.miracosta.cs112.finalproject.finalproject;
+
+public class Steve extends Hero {
+
+    /************** CONSTANTS ***************/
+    public static final String DEFAULT_NAME = "Steve";
+
+    public static final int DEFAULT_HEARTS = 10;
+
+    public static final Weapon DEFAULT_WEAPON = new Weapon();
+
+    /************** INSTANCE VARIABLES ***************/
+
+    /************** CONSTRUCTORS ***************/
+    /** Full Constructor
+     * @SabraReed
+     * @param name
+     * @param hearts
+     * @param weapon1
+     */
+    public Steve(String name, int hearts, Weapon weapon1) {
+        super(name, hearts, weapon1);
+    }
+
+    /** Default Constructor
+     * @SabraReed
+     */
+    public Steve() {
+        this(Steve.DEFAULT_NAME, Steve.DEFAULT_HEARTS, Steve.DEFAULT_WEAPON);
+    }
+
+    /** Copy Constructor
+     * @SabraReed
+     * @param original
+     */
+    public Steve(Steve original) {
+        super(original);
+    }
+
+    /************** OTHER METHODS ***************/
+}
